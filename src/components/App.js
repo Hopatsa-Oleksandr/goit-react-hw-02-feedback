d974062b71fc570d7c189a1008f0f36c8a06da01
Hopatsa-Oleksandr <bc.alexg@gmail.com>
2021-04-05 14:10:55 +0300

    комит

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Section from './Section/Section';
+import './App.css'
+
+export default function App() {
+  return (
+ <div className="mainContainer">
+       <Section title="Please leave feedback" />
+ </div>
+   
+  );
+}
---
+++ src/components/FeedbackOption/FeedbackOptions.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import s from './FeedbackOptions.module.css';
+
+export default class FeedbackOptions extends Component {
+    static defaultProps = {
+        options:[]
+    }
+    static propTypes = {
+        options: PropTypes.array.isRequired
+    }
+
+    render(){
+        const { options, onLeaveFeedback } = this.props;
+        return(
+            <div className={s.group_button}>
+                {options.map(({id, type, name}) => (
+                    <button
+                        type="button"
+                        className={s.button}
+                        key={id}
+                        onClick={() => onLeaveFeedback(type)}>{name}
+                    </button>
+                ))}
+            </div>
+        );
+    }
+}
+
